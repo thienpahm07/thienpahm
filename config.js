b242340bfeca6037c2518c11bb0a92c0c034c725
thienpahm07 <143874120+thienpahm07@users.noreply.github.com>
2023-12-07 22:15:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'HMV';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100050058608658';
 const eventName = 'HAPPY BIRTH DAY';
 const titleCard = 'Thân gửi hmv';
-const contentCard = 'Chúc m có 1 ngày sinh nhật vv và hạnh phúc,luôn luôn mỉm cười với người đời và bớt quạo lại nhé hh ';
+const contentCard = 'Chúc m có 1 ngày sinh nhật vv bên gia đình và người thân ,chúc m tuổi mới ngày càng xd và sớm thoát ế nhé';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
